865fc0ad9b42261c1b7f9937c42275a98ee539b8
Vladimir <woaouh@outlook.com>
2020-09-23 17:47:45 +0300

    Changed Page Title. Added H1 to Home Page. Fixed Styles in Form. Pre-Firebase-Auth commit.

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { Home } from './layouts/Home';
 function App() {
   return (
     <div className='container px-2'>
+      <h1 className='title has-text-centered mt-5 mb-0'>Notes App</h1>
       <Home />
     </div>
   );
